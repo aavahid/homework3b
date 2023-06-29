c16b2a43dab759d6da1c7a3771f2b4b9db2cd04b
vahidaa <vahidaa@code.edu.az>
2023-06-29 22:31:02 +0400

    last update

---
+++ HomeWork3B/HomeWork3B/Program.cs
@@ -0,0 +1,17 @@
+﻿int total = 0; // 1
+
+Console.WriteLine("Neçə ədəd girəcəksiniz? "); // 1
+int sum  = Convert.ToInt32(Console.ReadLine()); // 1
+
+for (int i = 1; i <= sum; i++) // n
+{
+	Console.Write($"{i}. ədəd : "); // 1
+	int x = Convert.ToInt32(Console.ReadLine()); // 1
+	total += x; // 1
+}
+
+double average = (double)total / sum; // 1
+
+Console.WriteLine("Ortalama: " + average); // 1
+
+// 1 + 1 + 1 + n + 1 + 1 + 1 + 1 + 1 = 8 + n = O(n)
